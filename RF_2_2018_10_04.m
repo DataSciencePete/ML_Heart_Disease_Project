1cb148ac1946e369166677f24419a7cfb71f44cf
Kevin Ryan <kevinryan@Kevins-MacBook-Pro-2.local>
2018-10-09 10:24:49 +0100

    Adding hyperparameter coptimisation code using Bayesian Optimisation

--- RF_2_2018_10_04.m
+++ RF_2_2018_10_04.m
@@ -38,7 +38,33 @@ toc
 
 
 %%
+% Bayesian Optimisation
 
+maxMinLS = 20;
+minLS = optimizableVariable('minLS',[1,maxMinLS],'Type','integer');
+numPTS = optimizableVariable('numPTS',[1,size(In,2)],'Type','integer'); % define number of predictors
+hyperparametersRF = [minLS; numPTS];
+
+% Also also consider tuning the number of trees in the ensemble
+
+
+function oobErr = oobErrRF(params,X)
+%oobErrRF Trains random forest and estimates out-of-bag quantile error
+%   oobErr trains a random forest of 300 regression trees using the
+%   predictor data in X and the parameter specification in params, and then
+%   returns the out-of-bag quantile error based on the median. X is a table
+%   and params is an array of OptimizableVariable objects corresponding to
+%   the minimum leaf size and number of predictors to sample at each node.
+randomForest = TreeBagger(300,X,'MPG','Method','regression',...
+    'OOBPrediction','on','MinLeafSize',params.minLS,...
+    'NumPredictorstoSample',params.numPTS);
+oobErr = oobQuantileError(randomForest);
+end
+
+results = bayesopt(@(params)oobErrRF(params,X),hyperparametersRF,...
+    'AcquisitionFunctionName','expected-improvement-plus','Verbose',0);
+
+%%
 % testmodel = TreeBagger(50,In,Out,...
 %           'Method','classification',...
 %           'OOBVarImp','On',...
