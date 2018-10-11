75453d5d0242c8b0be5fa148d7eca291521b6c0c
Peter Grimshaw <pagrimshaw@gmail.com>
2018-10-11 21:24:31 +0100

    debugged naivebayes.m and added comments

--- Naive_Bayes.m
+++ Naive_Bayes.m
@@ -1,41 +1,36 @@
-%[X, y] = load_heart_csv('heart.csv','numeric','table');
-[X,y] = loadheart('heart.csv');
+[X, y] = load_heart_csv('heart.csv','numeric','array');
 
-%% Set up Cross validation
-tic % time how long it takes process to run
-
-%%
+%Set up Cross validation
+%tic % time how long it takes process to run
 
 %data partition
-cp = cvpartition(X,'KFold',10); % Create 10-folds cross-validation partition for data. Each subsample has roughly equal size and roughly the same class proportions as in GROUP 
+cp = cvpartition(y,'KFold',10); % Create 10-folds cross-validation partition for data. Each subsample has roughly equal size and roughly the same class proportions as in GROUP
+
+%prediction function to be supplied to crossval function
+classF = @(XTRAIN,YTRAIN,XTEST)(predict(fitcnb(XTRAIN,YTRAIN),XTEST));
+
+order = unique(y); % Order of the group labels
+%confusionF = @(XTRAIN,YTRAIN,XTEST,YTEST)(confusionmat(YTEST,...
+%                                                       cellfun(@str2num,... % convert cell array of character vectors to a cell array of numerics
+%                                                       predict(fitcnb(XTRAIN,YTRAIN),XTEST)),...
+%                                                       'order', order));
+
+confusionF = @(XTRAIN,YTRAIN,XTEST,YTEST)(confusionmat(YTEST,predict(fitcnb(XTRAIN,YTRAIN),XTEST),'order', order));
+
+%%                                                   
+% missclassification error 
+missclasfError = crossval('mcr',X,y,'predfun',classF,'partition',cp);
+cfMat = crossval(confusionF,X,y,'partition',cp); % Matrix shows number of correctly and incorrectly classified samples for each classification for each of the 10 cross validated data sets
+cfMat = reshape(sum(cfMat),2,2); % summation of the 10 confusion matrices over the 10CV data sets
+% Generate confusion matrix
 
 %%
 
+%Note, I don't think this function works in Matlab R2017b, it doesn't seem
+%to recognise it
+%confusionchart(cfMat, {'Healthy'; 'Heart_Disease'})
 
-%prediction function to be supplied to crossval function
-classF = @(XTRAIN,YTRAIN,XTEST)(predict(fitncb(XTRAIN,YTRAIN),XTEST));
-
-order = unique(y); % Order of the group labels
-confusionF = @(XTRAIN,YTRAIN,XTEST,YTEST)(confusionmat(YTEST,...
-                                                       cellfun(@str2num,... % convert cell array of character vectors to a cell array of numerics
-                                                       predict(fitncb(XTRAIN,YTRAIN),XTEST)),...
-                                                       'order', order));
-% missclassification error 
-missclasfError = crossval('mcr',X,y,'predfun',classF,'partition',cp);
-cfMat = crossval(confusionF,In,Out,'partition',cp); % Matrix shows number of correctly and incorrectly classified samples for each classification for each of the 10 cross validated data sets
-cfMat = reshape(sum(cfMat),2,2); % summation of the 10 confusion matrices over the 10CV data sets
-% Generate confusion matrix
-confusionchart(cfMat, {'Healthy'; 'Heart_Disease'})
-toc
+%toc
 
 
-%% Cross validation
-rng(1); % fix seed to 1
-CVmdl = crossval(mdl);
-defaultLoss = kfoldLoss(CVmdl);
-
-%% Confusion matrix
-
-isLabels1 = resubPredict(Mdl1); %what is a resubstitution prediction?
-ConfusionMat1 = confusionmat(Y,isLabels1)
 
