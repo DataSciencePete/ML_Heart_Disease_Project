3ee616742e5ea61bb40c01d35f7014ad13e56417
kevjp <kevin@ryancodingdesign.com>
2018-11-06 11:06:58 +0000

    Added utility to add numPTS asa hyperparamter argument

--- myCVlossfcn.m
+++ myCVlossfcn.m
@@ -4,7 +4,7 @@ function loss = myCVlossfcn(params,In,Out,opts,cvp)
 % A = @(XTRAIN,YTRAIN,XTEST)(predict(TreeBagger(numTrees,XTRAIN,YTRAIN,'method','classification','Options',opts,...
 %  'MinLeafSize',params.minLS,'NumPredictorstoSample',params.numPTS), XTEST));
 A = @(XTRAIN,YTRAIN,XTEST)(predict(TreeBagger(params.numTrees,XTRAIN,YTRAIN,'method','classification', 'OOBPrediction','on', 'Options',opts,...
-  'MinLeafSize',params.minLS, 'NumPredictorsToSample', 1), XTEST));
+  'MinLeafSize',params.minLS, 'NumPredictorsToSample', params.numPTS), XTEST));
                                     
 loss = crossval('mcr',In,Out,'predfun',A,'partition',cvp);
 end
