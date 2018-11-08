989ac14feb6046540464e9f297384fef50f9d95e
kevjp <kevin@ryancodingdesign.com>
2018-11-08 09:23:32 +0000

    First working draft of final comparison script - corrected running errors

--- Model_Comparison.m
+++ Model_Comparison.m
@@ -1,4 +1,4 @@
-[train_features, train_labels, test_features, test_labels, X_header, cp] = load_heart_csv('heart.csv');
+[train_features, train_labels, test_features, test_labels, X_header, cp] = load_heart_csv('heart.csv','array','numeric');
 
 order = unique(train_labels); % Order of the group labels
 
@@ -11,17 +11,26 @@ NB_final_mdl = fitcnb(train_features,train_labels,...
 NB_train_time = toc;
 
 NB_confusion_mat = confusionmat(test_labels,...
-            predict(CNBMdl_final,test_features)...
+            predict(NB_final_mdl,test_features)...
             ,'Order', order);
 
-get_performance(NB_final_mdl,NB_confusion_mat);
-get_performance(RF_final_mdl,RF_confusion_mat);
+% Initialise figure plots
+figure;
+hold on;
+
+get_performance(NB_final_mdl,NB_confusion_mat, test_features, test_labels);
+
 fprintf('Naive Bayes train time %4.2fs\n',NB_train_time);
 
-[train_features, train_labels, test_features, test_labels, X_header, cp] = load_heart_csv('heart.csv','table');
+[train_features, train_labels, test_features, test_labels, X_header, cp] = load_heart_csv('heart.csv','table','categorical');
+
+order = unique(train_labels); % Order of the group labels(for categorical column)
 
 %Run final Random Forest Model
 In_high_imp_variables = removevars(train_features,{'age','trestbps','chol','fbs', 'restecg','exang','slope'});
+
+par = devicespec(); % see script file devicespec.m
+
 
 tic;
 RF_final_mdl = TreeBagger(109,In_high_imp_variables, train_labels,...
@@ -30,20 +39,25 @@ RF_final_mdl = TreeBagger(109,In_high_imp_variables, train_labels,...
                         'Options',par,...
                         'MinLeafSize',29,...
                         'NumPredictorsToSample', 1);
-rf_train_time = toc;
-fprintf('Random Forest train time %4.2fs\n',RF_train_time);
-                    
+RF_train_time = toc;
+
 RF_confusion_mat = confusionmat(test_labels,...
             categorical(...
             cellfun(@str2num,... % convert cell array of character vectors to a cell array of numerics
-            predict(final_mdl,test_features))),...
+            predict(RF_final_mdl,test_features))),...
             'order', order);
-                   
+        
+        
+        
 
+get_performance(RF_final_mdl,RF_confusion_mat, test_features, test_labels);
 
+fprintf('Random Forest train time %4.2fs\n',RF_train_time);
+  
+hold off;
 
 %Function to report model performance
-function get_performance(mdl,confusion_mat)
+function get_performance(mdl,confusion_mat,test_features, test_labels)
 
 recall = confusion_mat(1)/(confusion_mat(1)+ confusion_mat(3));
 precision = confusion_mat(1)/(confusion_mat(1) + confusion_mat(2));
@@ -51,11 +65,11 @@ F1 = (2*(precision * recall))/(precision + recall);
 specificity = confusion_mat(4)/(confusion_mat(4) + confusion_mat(3));
 accuracy = (confusion_mat(1) + confusion_mat(4))/sum([confusion_mat(1),confusion_mat(2),confusion_mat(3),confusion_mat(4)]);
 
-print('Recall %4.2f\n',recall);
-print('Precision %4.2f\n',precision);
-print('F1 %4.2f\n',F1);
-print('Specificity %4.2f\n',specificity);
-print('Accuracy %4.2f\n',accuracy);
+fprintf('Recall %4.2f\n',recall);
+fprintf('Precision %4.2f\n',precision);
+fprintf('F1 %4.2f\n',F1);
+fprintf('Specificity %4.2f\n',specificity);
+fprintf('Accuracy %4.2f\n',accuracy);
 
 % Draw ROC curve
 [yhat,scores,cost] = predict(mdl,test_features);
@@ -67,11 +81,12 @@ print('Accuracy %4.2f\n',accuracy);
 [fpr,tpr, T, AUC] = perfcurve(test_labels,scores(:,2), 1);
 
 % Plot ROC curve
-figure
+
 plot(fpr,tpr)
-hold
+
 xlabel('False Positive Rate')
 ylabel('True Positive Rate')
+
 
 end
 
