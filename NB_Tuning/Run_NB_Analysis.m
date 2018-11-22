e56b4730696178fbf59bacf80b52746c43b26ff5
Peter Grimshaw <pagrimshaw@gmail.com>
2018-11-22 20:11:01 +0000

    added comparison between best CE and MCR models and refactored get_performance

--- NB_Tuning/Run_NB_Analysis.m
+++ NB_Tuning/Run_NB_Analysis.m
@@ -1,7 +1,8 @@
 addpath(genpath('../'));
 [train_features, train_labels, test_features, test_labels, X_header, cp] = load_heart_csv('heart.csv','array','numeric');
+order = unique(train_labels); % Order of the group labels
 
-%Run grid search and Naive Bayes, testing normal and kernel distributions
+%% Run grid search and Naive Bayes, testing normal and kernel distributions
 %on the features and optimising the kernel width
 
 %Suppress warnings about standardising data before using kernel width
@@ -23,40 +24,73 @@ Naive_Bayes_optimisation(train_features_std,train_labels,cp)
 fprintf('Running grid search of features using kernel and normal distributions for each feature with standardisation\n')
 Naive_Bayes_man_gs(train_features_std,train_labels,X_header,cp)
 
+%% Take the model parameters identified by the CE metric and establish
+% performance for these models using accuracy as a metric on the test set
 
-%Take the model parameters identified by the CE metric and compute accuracy to see
-%if CE provides a better training metric
-
-%BayesOpt model
+%BayesOpt
 CNBMdl_CE_BO = fitcnb(train_features,train_labels,'DistributionNames','kernel','Width',44.651);
-confusion_mat_CE_BO = confusionmat(test_labels,predict(CNBMdl_final,test_features),'Order', order);
+confusion_mat_CE_BO = confusionmat(test_labels,predict(CNBMdl_CE_BO,test_features),'Order', order);
 fprintf('Best cross entropy trained model using Bayesopt for parameter search\n');
-get_performance(CNBMdl_CE_BO,confusion_mat_CE_BO,train_features, train_labels);
+get_performance(CNBMdl_CE_BO,confusion_mat_CE_BO,test_features, test_labels);
 
 %Grid search
 CNBMdl_CE_GS = fitcnb(train_features,train_labels,'DistributionNames','kernel','Width',36.337);
-confusion_mat_CE_GS = confusionmat(test_labels,predict(CNBMdl_final,test_features),'Order', order);
+confusion_mat_CE_GS = confusionmat(test_labels,predict(CNBMdl_CE_GS,test_features),'Order', order);
 fprintf('Best cross entropy trained model using Grid seach for parameter search\n');
-get_performance(CNBMdl_CE_GS,confusion_mat_CE_GS,train_features, train_labels);
+get_performance(CNBMdl_CE_GS,confusion_mat_CE_GS,test_features, test_labels);
 
 %manual grid search
 distributions_CE_manGS = {'kernel','mvmn','mvmn','kernel','normal','mvmn','mvmn','kernel','mvmn','normal','mvmn','mvmn','mvmn'};
 CNBMdl_CE_manGS = fitcnb(train_features,train_labels,'DistributionNames',distributions_CE_manGS);
-confusion_mat_CE_manGS = confusionmat(test_labels,predict(CNBMdl_final,test_features),'Order', order);
+confusion_mat_CE_manGS = confusionmat(test_labels,predict(CNBMdl_CE_manGS,test_features),'Order', order);
 fprintf('Best cross entropy trained model using manual Grid seach for parameter search\n');
-get_performance(CNBMdl_CE_manGS,confusion_mat_CE_manGS,train_features, train_labels);
+get_performance(CNBMdl_CE_manGS,confusion_mat_CE_manGS,test_features, test_labels);
 
+%The manual grid search gives significantly better performance. Test optimising
+%the kernel width to see if this gives any additional improvement
 
-%The best accuracy we can achieve using cross entropy as a training metric
-%is less than using MCR as a training metric. The above models all achieve
-%78.85% accuracy compared to 84.43% accuracy from using missclassification rate
-%as the training metric
-
-%Test optimising the kernel width to see if this gives any additional
-%improvement for the best result from the grid search using MCR as metric
-distributions = {'normal','mvmn','mvmn','kernel','normal','mvmn','mvmn','kernel','mvmn','normal','mvmn','mvmn','mvmn'};
 hpOO3 = struct('CVPartition',cp,'Verbose',2,'Optimizer','gridsearch');
+CVNBMdl_CE_manGS_optwidth = fitcnb(train_features,train_labels,'DistributionNames',distributions_CE_manGS, ...
+    'OptimizeHyperparameters',{'Width'},'HyperparameterOptimizationOptions',hpOO3);
+confusion_mat_CE_manGS_optwidth = confusionmat(test_labels,predict(CVNBMdl_CE_manGS_optwidth,test_features),'Order', order);
+fprintf('Best cross entropy trained model using manual grid seach with kernel width optimisation for parameter search\n');
+get_performance(CVNBMdl_CE_manGS_optwidth,confusion_mat_CE_manGS_optwidth,test_features, test_labels);
 
+%% Now compare this to doing the same thing using MCR
+
+%BayesOpt
+CNBMdl_MCR_BO = fitcnb(train_features,train_labels,'DistributionNames','kernel','Width',30.958);
+confusion_mat_MCR_BO = confusionmat(test_labels,predict(CNBMdl_MCR_BO,test_features),'Order', order);
+fprintf('Best MCR trained model using Bayesopt for parameter search\n');
+get_performance(CNBMdl_MCR_BO,confusion_mat_MCR_BO,test_features, test_labels);
+ 
+%Grid search
+CNBMdl_MCR_GS = fitcnb(train_features,train_labels,'DistributionNames','kernel','Width',36.337);
+confusion_mat_MCR_GS = confusionmat(test_labels,predict(CNBMdl_MCR_GS,test_features),'Order', order);
+fprintf('Best MCR trained model using grid seach for parameter search\n');
+get_performance(CNBMdl_MCR_GS,confusion_mat_MCR_GS,test_features, test_labels);
+ 
+%manual grid search
+distributions_MCR_manGS = {'normal','mvmn','mvmn','kernel','normal','mvmn','mvmn','kernel','mvmn','normal','mvmn','mvmn','mvmn'};
+CNBMdl_MCR_manGS = fitcnb(train_features,train_labels,'DistributionNames',distributions_MCR_manGS);
+confusion_mat_MCR_manGS = confusionmat(test_labels,predict(CNBMdl_MCR_manGS,test_features),'Order', order);
+fprintf('Best MCR trained model using manual grid seach for parameter search\n');
+get_performance(CNBMdl_MCR_manGS,confusion_mat_MCR_manGS,test_features, test_labels);
+ 
+%Test optimising the kernel width to see if this gives any additional improvement
+ 
+hpOO3 = struct('CVPartition',cp,'Verbose',2,'Optimizer','gridsearch');
+CVNBMdl_MCR_manGS_optwidth = fitcnb(train_features,train_labels,'DistributionNames',distributions_MCR_manGS, ...
+    'OptimizeHyperparameters',{'Width'},'HyperparameterOptimizationOptions',hpOO3);
+confusion_mat_MCR_manGS_optwidth = confusionmat(test_labels,predict(CVNBMdl_MCR_manGS_optwidth,test_features),'Order', order);
+fprintf('Best MCR trained model using manual grid seach with kernel width optimisation for parameter search\n');
+get_performance(CVNBMdl_MCR_manGS_optwidth,confusion_mat_MCR_manGS_optwidth,test_features, test_labels);
+
+
+%% Select final best model
+
+%Test the time to run optimise the kernel width 
+distributions = {'normal','mvmn','mvmn','kernel','normal','mvmn','mvmn','kernel','mvmn','normal','mvmn','mvmn','mvmn'};
 tic;
 CVNBMdl3 = fitcnb(train_features,train_labels,'DistributionNames',distributions, ...
     'OptimizeHyperparameters',{'Width'},'HyperparameterOptimizationOptions',hpOO3);
@@ -71,8 +105,6 @@ CNBMdl_final = fitcnb(train_features,train_labels,...
     'DistributionNames',distributions,'Width',36.337);
 final_model_train_time = toc;
 
-order = unique(train_labels); % Order of the group labels
-
 tic;
 confusion_mat = confusionmat(test_labels,predict(CNBMdl_final,test_features),'Order', order);
 final_model_test_time = toc;
@@ -85,5 +117,5 @@ if any(strcmp('Deep Learning Toolbox',{v.Name}))
 end
 
 %Plot ROC curve for NB final model
-get_performance(CNBMdl_final,confusion_mat,train_features, train_labels);
+get_performance(CNBMdl_final,confusion_mat,train_features,train_labels);
 
--- utils/get_performance.m
+++ utils/get_performance.m
@@ -1,5 +1,5 @@
 %Function to report model performance
-function [recall, precision, F1, specificity,accuracy, AUC] = get_performance(mdl,confusion_mat,train_features, train_labels)
+function [recall, precision, F1, specificity,accuracy, AUC] = get_performance(mdl,confusion_mat,features, labels)
 
 recall = confusion_mat(1)/(confusion_mat(1)+ confusion_mat(3));
 precision = confusion_mat(1)/(confusion_mat(1) + confusion_mat(2));
@@ -14,13 +14,13 @@ fprintf('Specificity %6.4f\n',specificity);
 fprintf('Accuracy %6.4f\n',accuracy);
 
 % Draw ROC curve
-[yhat,scores,cost] = predict(mdl,train_features);
+[yhat,scores,cost] = predict(mdl,features);
 
 %need to find NB method to calculate class scores
 %should be able to use predict function
 
 % calc fpr and tpr at different threshold as defined by T for ROC curve
-[fpr,tpr, T, AUC] = perfcurve(train_labels,scores(:,2), 1);
+[fpr,tpr, T, AUC] = perfcurve(labels,scores(:,2), 1);
 
 % Plot ROC curve
 
